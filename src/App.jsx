8f3d850e89ed09f1becbc949a944cbee8ebd5cdf
Tanim <tanim97@proton.me>
2026-05-26 20:08:43 +0600

    pic added

--- src/App.jsx
+++ src/App.jsx
@@ -23,7 +23,7 @@ function GlobalChrome({ preloaderDone }) {
   return (
     <>
       <Cursor />
-      <Navbar visible={navVisible} herName="her name" />
+      <Navbar visible={navVisible} herName="Nurin" />
       <MobileNav visible={navVisible} />
       <AudioManager />
     </>
